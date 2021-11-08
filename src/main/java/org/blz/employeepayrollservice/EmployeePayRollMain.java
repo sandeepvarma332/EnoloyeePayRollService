ecf11b0ce031b07f09b1d91d6f325a1e398e2030
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-11-08 14:28:26 +0530

    Added:Creating Employee Pay Roll

--- src/main/java/org/blz/employeepayrollservice/EmployeePayRollMain.java
+++ src/main/java/org/blz/employeepayrollservice/EmployeePayRollMain.java
@@ -3,7 +3,6 @@ package org.blz.employeepayrollservice;
 public class EmployeePayRollMain {
     public static void main(String[] args) {
         EmployeePayRollService employeePayrollService = new EmployeePayRollService();
-        System.out.println("Welcolme to employee payRoll");
         employeePayrollService.readEmployeeDataFromConsole();
         employeePayrollService.writeEmployeeDataInConsole();
     }
